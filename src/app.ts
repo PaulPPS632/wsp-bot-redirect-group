5349d7d167afa5e062a087da34f25b5ab191f546
PaulPPS632 <jeffertsap@hotmail.com>
2025-01-31 18:40:28 -0500

    ultimo cambio, se agrego ingenia y sp

--- src/app.ts
+++ src/app.ts
@@ -58,8 +58,9 @@ const main = async () => {
         for(const r of rutas){
           await bot.provider.sendImage(data.grupoJid,r,"");
         }
+        let mensaje = "";
         if(data.tipo_mensaje == "completa"){
-          const mensaje = [
+          mensaje = [
             `📅  FECHA DE MATRICULA : *${data.fecha_matricula ?? '-'}*`,
           `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
             `-------------------------------`,
@@ -91,11 +92,9 @@ const main = async () => {
             `📍 OBSERVACION:`,
             `*${data.observacion ?? '-'}*`
           ].join("\n");
-          await bot.provider.sendText(data.grupoJid,mensaje);
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          return res.end(JSON.stringify({ status: true}));
-        }else{
-          const mensaje = [
+
+        }else if(data.tipo_mensaje == "fraccionada"){
+          mensaje = [
             `📅  FECHA DE MATRICULA : *${data.fecha_matricula ?? '-'}*`,
           `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
             `-------------------------------`,
@@ -132,9 +131,168 @@ const main = async () => {
             `📍 OBSERVACION:`,
             `*${data.observacion ?? '-'}*`
           ].join("\n");
-          await bot.provider.sendText(data.grupoJid,mensaje);
+        }else if(data.tipo_mensaje == "fraccionada_sp"){
+          mensaje = [
+            `📅  FECHA DE SEPARACIÓN : *${data.fecha_matricula ?? '-'}*`,
+          `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
+            `-------------------------------`,
+            `☎  ASESOR : *${data.asesor ?? '-'}*`,
+            `NOMBRE Y APELLIDO : *${data.nombre ?? '-'}* *${data.apellido ?? '-'}*`,
+            `NUMERO : *${data.numero_celular ?? '-'}*`,
+            `DNI : *${data.dni ?? '-'}*`,
+            `CORREO : *${data.correo ?? '-'}*`,
+            `PROGRAMA : *${data.programa ?? '-'}*`,
+            `FECHA DE INICIO: *${data.fecha_inicio ?? '-'}*`,
+            `-------------------------------`,
+            `✅ TIPO SP. : ${data.tipo_sp ?? '-'}`,
+            `FECHA PAGO SP. : ${data.fecha_pago_sp ?? '-'}`,
+            `MONTO PAGO SP. : ${data.monto_pago_sp ?? '-'}`,
+            `-------------------------------`,
+            `✅ TIPO DE PAGO : *${data.tipo_pago ?? '-'}*`,
+            `TIPO DE VENTA : *${data.tipo_venta ?? '-'}*`,
+            `ESTADO 1ER PAGO : *${data.estado_1erpago ?? '-'}*`,
+            `FECHA 1ER PAGO : *${data.fecha_1erpago ?? '-'}*`,
+            `MONTO 1ER PAGO : *${data.monto_1erpago ?? '-'}*`,
+            ``,
+            `ESTADO 2DO PAGO: *${data.estado_2dopago ?? '-'}*`,
+            `FECHA 2DO PAGO : *${data.fecha_2dopago ?? '-'}*`,
+            `MONTO 2DO PAGO : *${data.monto_2dopago ?? '-'}* `,
+            ``,
+            `🎁 INCLUYE PROGRAMA DE REGALO : *${data.incluye_programa ?? '-'}*`,
+            `🎁 PROGRAMA DE REGALO : *${data.nombre_programa ?? '-'}*`,
+            `-------------------------------`,
+            `CEL : *${data.cel ?? '-'}*`,
+            `CIP : *${data.cip ?? '-'}*`,
+            `CARNET : *${data.carnet ?? '-'}*`,
+            `AUTODESK : *${data.autodesk ?? '-'}*`,
+            `PMI : *${data.pmi ?? '-'}*`,
+            `CERTI. FISICO CCD : *${data.cert_fisico ?? '-'}*`,
+            `CERTI: DIGITAL CCD : *${data.cert_digital ?? '-'}*`,
+            `PREPARACION LICENCIAMIENTO : *${data.preparacion_licen ?? '-'}*`,
+            `-------------------------------`,
+            `📍 OBSERVACION:`,
+            `*${data.observacion ?? '-'}*`
+          ].join("\n");
+        }else if(data.tipo_mensaje == "completa_sp"){
+          mensaje = [
+            `📅  FECHA DE SEPARACIÓN : *${data.fecha_matricula ?? '-'}*`,
+          `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
+            `-------------------------------`,
+            `☎  ASESOR : *${data.asesor ?? '-'}*`,
+            `NOMBRE Y APELLIDO : *${data.nombre ?? '-'}* *${data.apellido ?? '-'}*`,
+            `NUMERO : *${data.numero_celular ?? '-'}*`,
+            `DNI : *${data.dni ?? '-'}*`,
+            `CORREO : *${data.correo ?? '-'}*`,
+            `PROGRAMA : *${data.programa ?? '-'}*`,
+            `FECHA DE INICIO: *${data.fecha_inicio ?? '-'}*`,
+            `-------------------------------`,
+            `✅ TIPO SP. : ${data.tipo_sp ?? '-'}`,
+            `FECHA PAGO SP. : ${data.fecha_pago_sp ?? '-'}`,
+            `MONTO PAGO SP. : ${data.monto_pago_sp ?? '-'}`,
+            `-------------------------------`,
+            `✅ TIPO DE PAGO : *${data.tipo_pago ?? '-'}*`,
+            `TIPO DE VENTA : *${data.tipo_venta ?? '-'}*`,
+            `ESTADO PAGO : *${data.estado_1erpago ?? '-'}*`,
+            `FECHA PAGO : *${data.fecha_1erpago ?? '-'}*`,
+            `MONTO PAGO : *${data.monto_1erpago ?? '-'}*`,
+            ``,
+            `🎁 INCLUYE PROGRAMA DE REGALO : *${data.incluye_programa ?? '-'}*`,
+            `🎁 PROGRAMA DE REGALO : *${data.nombre_programa ?? '-'}*`,
+            `-------------------------------`,
+            `CEL : *${data.cel ?? '-'}*`,
+            `CIP : *${data.cip ?? '-'}*`,
+            `CARNET : *${data.carnet ?? '-'}*`,
+            `AUTODESK : *${data.autodesk ?? '-'}*`,
+            `PMI : *${data.pmi ?? '-'}*`,
+            `CERTI. FISICO CCD : *${data.cert_fisico ?? '-'}*`,
+            `CERTI: DIGITAL CCD : *${data.cert_digital ?? '-'}*`,
+            `PREPARACION LICENCIAMIENTO : *${data.preparacion_licen ?? '-'}*`,
+            `-------------------------------`,
+            `📍 OBSERVACION:`,
+            `*${data.observacion ?? '-'}*`
+          ].join("\n");
+        }else if(data.tipo_mensaje == "deuda"){
+          mensaje = [
+            `📅  FECHA DE PAGO DEUDA : *${data.fecha_matricula ?? '-'}*`,
+          `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
+            `-------------------------------`,
+            `☎  ASESOR : *${data.asesor ?? '-'}*`,
+            `NOMBRE Y APELLIDO : *${data.nombre ?? '-'}* *${data.apellido ?? '-'}*`,
+            `NUMERO : *${data.numero_celular ?? '-'}*`,
+            `DNI : *${data.dni ?? '-'}*`,
+            `PROGRAMA : *${data.programa ?? '-'}*`,
+            `-------------------------------`,
+            `✅ TIPO DE PAGO : *${data.tipo_pago ?? '-'}*`,
+            `TIPO DE VENTA : *${data.tipo_venta ?? '-'}*`,
+            `ESTADO 1ER PAGO : *${data.estado_1erpago ?? '-'}*`,
+            `FECHA 1ER PAGO : *${data.fecha_1erpago ?? '-'}*`,
+            `MONTO 1ER PAGO : *${data.monto_1erpago ?? '-'}*`,
+            ``,
+            `ESTADO 2DO PAGO : *${data.estado_2dopago ?? '-'}*`,
+            `FECHA 2DO PAGO : *${data.fecha_2dopago ?? '-'}*`,
+            `MONTO 2DO PAGO : *${data.monto_2dopago ?? '-'}*`,
+            ``,
+            `🎁 INCLUYE PROGRAMA DE REGALO : *${data.incluye_programa ?? '-'}*`,
+            `🎁 PROGRAMA DE REGALO : *${data.nombre_programa ?? '-'}*`,
+            `-------------------------------`,
+            `📍 OBSERVACION:`,
+            `*${data.observacion ?? '-'}*`
+          ].join("\n");
+        }else if(data.tipo_mensaje == "separacion_ingenia"){
+          mensaje = [
+            `📅  FECHA DE MATRICULA : *${data.fecha_matricula ?? '-'}*`,
+            `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
+            `☎  ASESOR : *${data.asesor ?? '-'}*`,
+            `-------------------------------`,
+            `NOMBRE Y APELLIDO APODERADO: *${data.nombre_apoderado ?? '-'}* *${data.apellido_apoderado ?? '-'}*`,
+            `NUMERO APODERADO: *${data.numero_apoderado ?? '-'}*`,
+            '',
+            `NOMBRE Y APELLIDO ALUMNO: *${data.nombre_alumno ?? '-'}* *${data.apellido_alumno ?? '-'}*`,
+            `NUMERO ALUMNO: *${data.celular_alumno ?? '-'}*`,
+            `DNI : *${data.dni_alumno ?? '-'}*`,
+            `CORREO : *${data.correo_alumno ?? '-'}*`,
+            `CURSO : *${data.curso ?? '-'}*`,
+            `FECHA DE INICIO : *${data.fecha_inicio ?? '-'}*`,
+            `-------------------------------`,
+            `✅ TIPO DE VENTA : *${data.tipo_venta ?? '-'}*`,
+            `ESTADO SEPARACION : *${data.estado_separacion ?? '-'}*`,
+            `FECHA SEPARACION : *${data.fecha_separacion ?? '-'}*`,
+            `MONTO SEPARACION : *${data.monto_separacion ?? '-'}*`,
+            ``,
+            `ESTADO PROX. PAGO : *${data.estado_prox_pago ?? '-'}*`,
+            `FECHA PROX. PAGO : *${data.fecha_prox_pago ?? '-'}*`,
+            `MONTO PROX. PAGO : *${data.monto_prox_pago ?? '-'}*`,
+            `-------------------------------`,
+            `📍 OBSERVACION:`,
+            `*${data.observacion ?? '-'}*`
+          ].join("\n");
+        }else if(data.tipo_mensaje == "completa_ingenia"){
+          mensaje = [
+            `📅 FECHA DE MATRICULA : *${data.fecha_matricula ?? '-'}*`,
+            `🟢 CODIGO CAMPAÑA : *${data.codigo ?? '-'}*`,
+            `☎  ASESOR : *${data.asesor ?? '-'}*`,
+            `-------------------------------`,
+            `NOMBRE Y APELLIDO APODERADO: *${data.nombre_apoderado ?? '-'}* *${data.apellido_apoderado ?? '-'}*`,
+            `NUMERO APODERADO: *${data.numero_apoderado ?? '-'}*`,
+            '',
+            `NOMBRE Y APELLIDO ALUMNO: *${data.nombre_alumno ?? '-'}* *${data.apellido_alumno ?? '-'}*`,
+            `NUMERO ALUMNO: *${data.celular_alumno ?? '-'}*`,
+            `DNI : *${data.dni_alumno ?? '-'}*`,
+            `CORREO : *${data.correo_alumno ?? '-'}*`,
+            `CURSO : *${data.curso ?? '-'}*`,
+            `FECHA DE INICIO : *${data.fecha_inicio ?? '-'}*`,
+            `-------------------------------`,
+            `✅ TIPO DE VENTA : *${data.tipo_venta ?? '-'}*`,
+            `ESTADO PAGO : *${data.estado_pago ?? '-'}*`,
+            `FECHA PAGO : *${data.fecha_pago ?? '-'}*`,
+            `MONTO PAGO : *${data.monto_pago ?? '-'}*`,
+            `-------------------------------`,
+            `📍 OBSERVACION:`,
+            `*${data.observacion ?? '-'}*`
+          ].join("\n");
         }
-        res.writeHead(200, { 'Content-Type': 'application/json' })
+        await bot.provider.sendText(data.grupoJid,mensaje);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         return res.end(JSON.stringify({ status: true}));
       } catch (error) {
         console.log(error)
